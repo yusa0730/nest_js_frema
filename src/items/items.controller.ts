e51ea40759cc6df92a17dd01f611f5fdc9f76eb5
yusa0730 <isizawa5226@gmail.com>
2023-06-29 10:57:14 +0900

    add: crud処理追加

---
+++ src/items/items.controller.ts
@@ -0,0 +1,31 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ItemStatus } from './item-status.enum';
+import { Item } from './item.model';
+import { ItemsService } from './items.service';
+
+@Controller('items')
+export class ItemsController {
+  constructor(private readonly itemsService: ItemsService) {};
+
+  @Get()
+  findAll():Item[] {
+    return this.itemsService.findAll();
+  }
+
+  @Post()
+  create(
+    @Body('id') id: string,
+    @Body('name') name: string,
+    @Body('price') price: number,
+    @Body('description') description: string,
+  ): Item {
+    const item: Item = {
+      id,
+      name,
+      price,
+      description,
+      status: ItemStatus.ON_SALE,
+    }
+    return this.itemsService.create(item);
+  }
+}
---
+++ src/items/items.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { Item } from './item.model';
+
+@Injectable()
+export class ItemsService {
+  private items: Item[] = [];
+
+  findAll():Item[] {
+    return this.items;
+  }
+
+  create(item: Item): Item {
+    this.items.push(item);
+    return item;
+  }
+}
